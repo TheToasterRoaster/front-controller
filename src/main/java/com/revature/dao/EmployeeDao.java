8744cf0f9aaec33e35b3e15f8996cb7108a6d84d
Mark Kolb <markkolb731@gmail.com>
2022-03-08 15:47:01 -0500

    fix GetHelper in revature.web package

---
+++ src/main/java/com/revature/dao/EmployeeDao.java
@@ -0,0 +1,42 @@
+package com.revature.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.models.Employee;
+import com.revature.util.HibernateUtil;
+
+public class EmployeeDao {
+
+	public int insert(Employee e) {
+		
+		Session ses = HibernateUtil.getSession();
+		
+		Transaction tx = ses.beginTransaction();
+		
+		int pk = (int) ses.save(e);
+		
+		tx.commit();
+		
+		return pk;
+	}
+	
+	public List<Employee> findAll() {
+		
+		Session ses = HibernateUtil.getSession();
+		
+		List<Employee> emps = ses.createQuery("from Employee", Employee.class).list();
+	
+		return emps;
+	
+	}
+	
+	public boolean delete(int id) {
+		return false;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/revature/web/RequestHelper.java
@@ -0,0 +1,17 @@
+package com.revature.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RequestHelper {
+
+	public static void processLogin(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		
+		protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		
+	}
+	
+}
